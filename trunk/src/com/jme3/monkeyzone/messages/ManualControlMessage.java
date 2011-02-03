4d7f41581f7ea5fd3ab8299af21d9849f3bb8c54
normen667 <normen667@1af95cfd-333b-63c4-1e1b-63e36685f83d>
2011-02-03 00:15:13 +0000

    - small fix

--- trunk/src/com/jme3/monkeyzone/messages/ManualControlMessage.java
+++ trunk/src/com/jme3/monkeyzone/messages/ManualControlMessage.java
@@ -75,7 +75,7 @@ public class ManualControlMessage extends PhysicsSyncMessage {
     public void applyData(Object object) {
         NetworkedManualControl netControl = ((Spatial) ((PhysicsCollisionObject) object).getUserObject()).getControl(NetworkedManualControl.class);
         if (netControl != null) {
-            if (netControl.getSyncManager() != null) {
+            if (netControl.getSyncManager().getServer() != null) {
                 netControl.getSyncManager().broadcast(this);
             }
             netControl.doMoveX(moveX);
