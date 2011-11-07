ad8d87c5f9b2eff8db99af09276c53dd816f0bab
normen667 <normen667@1af95cfd-333b-63c4-1e1b-63e36685f83d>
2011-11-07 00:02:33 +0000

    - fix MonkeyZone for jME3 beta

--- src/com/jme3/monkeyzone/messages/HandshakeMessage.java
+++ src/com/jme3/monkeyzone/messages/HandshakeMessage.java
@@ -31,7 +31,7 @@
  */
 package com.jme3.monkeyzone.messages;
 
-import com.jme3.network.message.Message;
+import com.jme3.network.AbstractMessage;
 import com.jme3.network.serializing.Serializable;
 
 /**
@@ -40,7 +40,7 @@ import com.jme3.network.serializing.Serializable;
  * @author normenhansen
  */
 @Serializable()
-public class HandshakeMessage extends Message {
+public class HandshakeMessage extends AbstractMessage {
 
     public int protocol_version;
     public int client_version;
